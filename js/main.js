004fd17e50b473990141cb6119b78f52c565ade0
adevlinb <devlinbgenassembly@gmail.com>
2022-10-25 22:21:55 -0700

    all posts showing of selected user

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@
 /*----- app's state (variables) -----*/
 let users;
 let posts;
-
+let selectedUser;
 
 
 
 
 /*----- cached element references -----*/
 let usersDiv = document.getElementById("users")
-
-
+let postsDiv = document.getElementById("posts")
+let postsHeaderEl = document.getElementById("selectedUser")
 
 
 
@@ -34,39 +34,56 @@ init();
 
 async function init() {
     // get all users
+    console.log("test")
     users = [];
     posts = [];
     await fetch('https://jsonplaceholder.typicode.com/users')
         .then((response) => response.json())
         .then((json) => users = json);
-
-
-    render();
+    selectedUser = users[0]
+    renderUsers();
+    renderPosts();
 }
 
-function render() {
-    console.log(users)
+function renderUsers() {
+    console.log(users, posts)
     users.forEach(function(user, idx) {
-        let title = document.createElement("h1");
+        let title = document.createElement("h3");
         title.innerHTML = user.name
         title.id = idx
         usersDiv.appendChild(title)
     });
-    // posts.forEach(function(user, idx) {
-    //     let title = document.createElement("h1");
-    //     title.innerHTML = user.name
-    //     title.id = idx
-    //     usersDiv.appendChild(title)
-    // });
-
 }
 
-async function handleUserClick(evt) {
-    console.log(evt, users[evt.target.id].id, evt.target.id);
+function renderPosts() {
+    if (posts.length === 0) {
+        postsHeaderEl.textContent = "Please Select A User On The Left"
+    } else {
+        console.log(selectedUser.name)
+        postsHeaderEl.textContent = selectedUser.name
+        posts.forEach(function(post, idx) {
+            let title = document.createElement("h3");
+            title.innerHTML = post.title
+            postsDiv.appendChild(title)
+        });
+    }
+}
 
-    // await fetch(`https://jsonplaceholder.typicode.com/posts/${users[evt.target.id].id}`)
+function clearPreviousPosts() {
+    while (postsDiv.firstChild) {
+        postsDiv.removeChild(postsDiv.firstChild)
+    }
+}
+
+
+async function handleUserClick(evt) {
+    console.log(evt.target.id)
+    if(evt.target.id === "users") return
+    selectedUser = users[evt.target.id]
+    posts = [];
+    clearPreviousPosts();
     await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${users[evt.target.id].id}`)
         .then((response) => response.json())
-        .then((json) => console.log(json, "posts"));
-
+        .then((json) => posts = json);
+    renderPosts();
 }
